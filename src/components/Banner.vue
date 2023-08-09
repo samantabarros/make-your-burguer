976fff7e9d132bffd5ff34904be3499035c61e8e
samanta <samantadesenvolvedora@gmail.com>
2023-08-09 12:00:01 -0300

    Banner da aplicação

---
+++ src/components/Banner.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id = "main-banner">
+        <h1>Make Your Burguer</h1>
+    </div> 
+</template>
+
+
+<script>
+export default{
+    name: "Banner"
+}
+</script>
+
+<style scoped>
+    #main-banner{
+        background-image: url('/public/img/burger.jpg');
+        background-size: cover;
+        background-position: 0 -250px;
+        display: flex;
+        align-items: center;
+        height: 500px;
+        justify-content: flex-start;
+    }
+
+    #main-banner h1{
+        color: #fff;
+        text-align: center;
+        font-size: 60px;
+        padding: 20px 40px;
+    }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,18 @@
 <template>
+  <Banner/>
   <div class="main-container">
-    <h1>HOME</h1>
+    <h1>Monte o seu burguer: </h1>
   </div>
 </template>
 
 <script>
 
+import Banner from "../components/Banner.vue"
+
 export default {
   name: 'Home',
   components: {
+    Banner
   }
 }
 </script>
